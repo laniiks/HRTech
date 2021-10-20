9d46c0a0a40c1d34442c19525191a1d2d26edb7a
laniiks <kotov.kolya2013@mail.ru>
2021-10-20 21:42:28 +0300

    Добавил миграции

---
+++ HRTech-WebApi/src/HRTech.Infrastructure/Migrations/20211020181241_AddPhoneNumberAndPatronymicInUser.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HRTech.Infrastructure.Migrations
+{
+    public partial class AddPhoneNumberAndPatronymicInUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Patronymic",
+                table: "AspNetUsers",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Patronymic",
+                table: "AspNetUsers");
+        }
+    }
+}
