fb62bab0f1ff40ec68318fb2945cada8faea225d
Котов Николай Алексеевич <n.kotov@solarl.ru>
2021-12-04 18:28:58 +0300

    +

--- HRTech-WebApi/src/HRTech.WebApi/Startup.cs
+++ HRTech-WebApi/src/HRTech.WebApi/Startup.cs
@@ -176,10 +176,11 @@ namespace HRTech.WebApi
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "HRTech.WebApi v1"));
+                
             }
-
+            
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "HRTech.WebApi v1"));
             app.UseHttpsRedirection();
 
             app.UseRouting();
